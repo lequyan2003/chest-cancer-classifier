3668cab2ec10486d42d8f560fde363654e93403e
lequyan2003 <lequyan2003@gmail.com>
2024-07-07 22:50:23 -0400

    data ingestion finished

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 from chestCancerClassifier import logger
+from chestCancerClassifier.pipeline.stage_01_data_ingestion import \
+    DataIngestionTrainingPipeline
 
-logger.info("Welcome to Chest Cancer Classifier!")
+STAGE_NAME = "Data Ingestion stage"
+
+try:
+    logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(
+        f">>>>>>> stage {STAGE_NAME} completed <<<<<<<\n\nx==========x"
+    )
+except Exception as e:
+    logger.exception(e)
+    raise e
--- template.py
+++ template.py
@@ -15,7 +15,7 @@ list_of_files = [
     f"src/{project_name}/config/configuration.py",
     f"src/{project_name}/pipeline/__init__.py",
     f"src/{project_name}/entity/__init__.py",
-    f"src/{project_name}/constants/__init.py__",
+    f"src/{project_name}/constants/__init__.py",
     "config/config.yaml",
     "dvc.yaml",
     "params.yaml",
