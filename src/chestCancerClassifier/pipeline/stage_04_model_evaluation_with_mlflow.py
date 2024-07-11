0c42e73d5973076bc2d52cf47cc1e510990ca35a
lequyan2003 <lequyan2003@gmail.com>
2024-07-10 20:49:55 -0400

    dvc added

--- src/chestCancerClassifier/pipeline/stage_04_model_evaluation_with_mlflow.py
+++ src/chestCancerClassifier/pipeline/stage_04_model_evaluation_with_mlflow.py
@@ -1,3 +1,5 @@
+import os
+
 from chestCancerClassifier import logger
 from chestCancerClassifier.components.model_evaluation_with_mlflow import \
     Evaluation
@@ -15,10 +17,12 @@ class EvaluationPipeline:
         eval_config = config.get_evaluation_config()
         evaluation = Evaluation(eval_config)
         evaluation.evaluation()
-        evaluation.log_into_mlflow()
+        # evaluation.log_into_mlflow()
 
 
 if __name__ == "__main__":
+    os.environ["repo_owner"] = "lequyan2003"
+    os.environ["repo_name"] = "chest-cancer-classifier"
     try:
         logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<<")
         obj = EvaluationPipeline()
